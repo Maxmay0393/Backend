b73596ad9693807232d3da945400613d368b645a
Maksim Shitikov <maksik0393@gmail.com>
2015-05-18 17:24:59 +0300

    getNewsCollection() delete

--- app/code/local/DS/News/Block/News.php
+++ app/code/local/DS/News/Block/News.php
@@ -3,12 +3,7 @@
 class DS_News_Block_News extends Mage_Core_Block_Template
 {
 
-    public function getNewsCollection()
-    {
-        $newsCollection = Mage::getModel('dsnews/news')->getCollection();
-        $newsCollection->setOrder('news_id', 'DESC');
-        return $newsCollection;
-    }
+    
 	 public function getWelcome()
     {
         if (empty($this->_data['welcome'])) {
