6782fe7b806df599dd2e734e02e2de7b3de38295
Maksim Shitikov <maksik0393@gmail.com>
2015-05-18 17:46:06 +0300

    viewAction delete

--- app/code/local/DS/News/controllers/IndexController.php
+++ app/code/local/DS/News/controllers/IndexController.php
@@ -5,28 +5,14 @@ class DS_News_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction()
     {
         $this->loadLayout();
-        $this->getLayout()->getBlock('head')->setTitle($this->__(Mage::getStoreConfig('my/my_group/my_input',Mage::app()->getStore())));
-        $this->getLayout()->getBlock('head')->setDescription($this->__(Mage::getStoreConfig('my/my_group/my_input2',Mage::app()->getStore())));
-        $this->getLayout()->getBlock('head')->setKeywords($this->__(Mage::getStoreConfig('my/my_group/my_input1',Mage::app()->getStore())));
+        $this->getLayout()->getBlock('head')->setTitle($this->__(Mage::getStoreConfig('ds/ds_group/ds_title',Mage::app()->getStore())));
+        $this->getLayout()->getBlock('head')->setDescription($this->__(Mage::getStoreConfig('ds/ds_group/ds_description',Mage::app()->getStore())));
+        $this->getLayout()->getBlock('head')->setKeywords($this->__(Mage::getStoreConfig('ds/ds_group/ds_keywords',Mage::app()->getStore())));
         $this->renderLayout();
 
     }
 
-    public function viewAction()
-    {
-//        $newsId = Mage::app()->getRequest()->getParam('id', 0);
-//        $news = Mage::getModel('dsnews/news')->load($newsId);
-//
-//        if ($news->getId() > 0) {
-            $this->loadLayout();
-//            $this->getLayout()->getBlock('news.content')->assign(array(
-//                "newsItem" => $news,
-//            ));
-            $this->renderLayout();
-//        } else {
-//            $this->_forward('noRoute');
-//        }
-    }
+   
 	
 	public function saveAction()
  {
