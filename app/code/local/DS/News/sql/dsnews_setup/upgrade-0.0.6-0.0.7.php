e08c694f683c0e799c89292439515fe368dc6735
Maksim Shitikov <maksik0393@gmail.com>
2015-05-20 12:33:06 +0300

    add foreignkey sql

--- app/code/local/DS/News/sql/dsnews_setup/upgrade-0.0.6-0.0.7.php
+++ app/code/local/DS/News/sql/dsnews_setup/upgrade-0.0.6-0.0.7.php
@@ -2,14 +2,19 @@
 
 //echo '<h1>Upgrade DS News to version 0.0.3</h1>';
 //exit;
+
 $installer = $this;
-$tableNews = $installer->getTable('dsnews/table_news');
+
 
 $installer->startSetup();
-$installer->run
-("
-   ALTER TABLE `{$tableNews}` ADD  FOREIGN KEY (`title`) REFERENCES `customer_entity`(`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+$installer->getConnection()
+    ->addForeignKey
+	(
+	$installer->getFkName('ds_news_entities', 'title','customer/entity', 'entity_id'),
+       $installer->getTable('ds_news_entities'),'title', $installer->getTable('customer/entity'), 'entity_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE
+);
 
-");
+
 $installer->endSetup();
 
